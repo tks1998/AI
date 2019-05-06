5711f6314781b621d29e96064d3c17217b1b7799
tks1998 <trankimsen1681998@gmail.com>
2019-05-06 15:45:47 +0700

    create project

---
+++ src/app/component_board/board.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'board',
+    templateUrl: '../component_board/board.html',
+    styleUrls: ['../component_board/board.css']
+//    providers: [ComputeService]
+})
+
+export class BoardComponent{}
